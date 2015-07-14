dc7003458fd198fa0f59d0ce621d86cf284c3e37
alex <zxfhacker@gmail.com>
2015-07-14 23:26:39 +0800

    add fouction search stock by autoCompleteText widget

--- app/src/main/java/com/alex/develop/stockanalyzer/SearchActivity.java
+++ app/src/main/java/com/alex/develop/stockanalyzer/SearchActivity.java
@@ -2,8 +2,23 @@ package com.alex.develop.stockanalyzer;
 
 import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.BaseAdapter;
+import android.widget.CompoundButton;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.alex.develop.entity.Stock;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by alex on 15-6-15.
@@ -16,10 +31,144 @@ public class SearchActivity extends BaseActivity {
         setContentView(R.layout.search_activity);
 
         ActionBar actionBar = getActionBar();
-        if(null != actionBar) {
+        if (null != actionBar) {
             actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
             actionBar.setCustomView(R.layout.search_action_bar_titile);
         }
+
+        Analyzer analyzer = (Analyzer) getApplication();
+
+        SearchAdapter adapter = new SearchAdapter(analyzer.getStockList());
+
+        AutoCompleteTextView stockSearch = (AutoCompleteTextView) findViewById(R.id.stockSearch);
+        stockSearch.setAdapter(adapter);
+
     }
 
+    private static class ViewHolder {
+        ToggleButton stockCollectBtn;
+        TextView stockCode;
+        TextView stockName;
+    }
+
+    private class SearchAdapter extends BaseAdapter implements Filterable {
+
+        public SearchAdapter(List<Stock> stocks) {
+            this.stocks = stocks;
+        }
+
+        @Override
+        public Filter getFilter() {
+
+            if (null == filter) {
+                filter = new StockFilter();
+            }
+
+            return filter;
+        }
+
+        @Override
+        public int getCount() {
+            return stocks.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return stocks.get(position).getName();
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            final Stock stock = stocks.get(position);
+            ViewHolder holder;
+
+            if (null == convertView) {
+                convertView = LayoutInflater.from(SearchActivity.this).inflate(R.layout.stock_search_item, null);
+
+                holder = new ViewHolder();
+                holder.stockCollectBtn = (ToggleButton) convertView.findViewById(R.id.stockCollectBtn);
+
+                holder.stockCollectBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        stock.setCollect(isChecked ? 1 : 0);
+                    }
+                });
+
+                holder.stockCode = (TextView) convertView.findViewById(R.id.stockCode);
+                holder.stockName = (TextView) convertView.findViewById(R.id.stockName);
+
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            if (stock.isCollected()) {
+                holder.stockCollectBtn.setChecked(true);
+            }
+
+            holder.stockCode.setText(stock.getCode());
+            holder.stockName.setText(stock.getName());
+            return convertView;
+        }
+
+        private List<Stock> stocks;
+        private List<Stock> originalStocks;
+        private final Object mLock = new Object();
+        private StockFilter filter;
+
+        private class StockFilter extends Filter {
+
+            @Override
+            protected FilterResults performFiltering(CharSequence prefix) {
+                FilterResults results = new FilterResults();
+
+                if (null == originalStocks) {
+                    synchronized (mLock) {
+                        originalStocks = new ArrayList<>(stocks);
+                    }
+                }
+
+                if (null == prefix || 0 == prefix.length()) {
+                    synchronized (mLock) {
+                        ArrayList<Stock> list = new ArrayList<>(originalStocks);
+                        results.values = list;
+                        results.count = list.size();
+                    }
+                } else {
+                    String prefixStr = prefix.toString().toLowerCase();
+                    final List<Stock> values = originalStocks;
+                    final List<Stock> newValues = new ArrayList<>();
+                    for (Stock stock : values) {
+                        String code = stock.getCode();
+                        if (code.startsWith(prefixStr) || code.contains(prefixStr)) {
+                            newValues.add(stock);
+                        }
+                    }
+
+                    results.values = newValues;
+                    results.count = newValues.size();
+                }
+
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                stocks = (List<Stock>) results.values;
+
+                if (0 < results.count) {
+                    notifyDataSetChanged();
+                } else {
+                    notifyDataSetInvalidated();
+                }
+            }
+        }
+    }
 }
