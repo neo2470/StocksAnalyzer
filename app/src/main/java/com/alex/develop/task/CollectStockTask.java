7ce13c5ce0475aca00e287e2d6aa58686bfc4ef3
Alex <zxfhacker@gmail.com>
2015-07-16 18:35:21 +0800

    change nothing

--- app/src/main/java/com/alex/develop/task/CollectStockTask.java
+++ app/src/main/java/com/alex/develop/task/CollectStockTask.java
@@ -4,7 +4,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.alex.develop.entity.Stock;
