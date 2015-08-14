cdcb19a35180065ee63bb112ebdc07bb56883d58
Alex <zxfhacker@gmail.com>
2015-08-14 18:50:57 +0800

    add logic code for custom Keyboard

--- app/src/main/java/com/alex/develop/stockanalyzer/SearchActivity.java
+++ app/src/main/java/com/alex/develop/stockanalyzer/SearchActivity.java
@@ -4,16 +4,18 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
+import android.inputmethodservice.KeyboardView.OnKeyboardActionListener;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.BaseAdapter;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.TextView;
@@ -23,7 +25,6 @@ import com.alex.develop.entity.Stock;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Created by alex on 15-6-15.
@@ -52,12 +53,21 @@ public class SearchActivity extends BaseActivity {
         setResult(Activity.RESULT_OK);
 
         // 自定义键盘
-        Keyboard keyboard = new Keyboard(this, R.xml.qwerty);
-        KeyboardView keyboardView = (KeyboardView) findViewById(R.id.keyboardView);
-        keyboardView.setKeyboard(keyboard);
-        keyboardView.setPreviewEnabled(false);
+        symbols = new Keyboard(this, R.xml.symbols);
+        qwerty = new Keyboard(this, R.xml.qwerty);
+        mKeyboard = (KeyboardView) findViewById(R.id.keyboardView);
+        mKeyboard.setKeyboard(symbols);
+        mKeyboard.setOnKeyboardActionListener(onKeyboardActionListener);
+    }
 
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    private void showKeyboard() {
+        mKeyboard.setVisibility(View.VISIBLE);
+        mKeyboard.setEnabled(true);
+    }
+
+    private void hideKeyboard() {
+        mKeyboard.setVisibility(View.GONE);
+        mKeyboard.setEnabled(false);
     }
 
     private class SearchAdapter extends BaseAdapter implements Filterable {
@@ -185,9 +195,118 @@ public class SearchActivity extends BaseActivity {
         private StockFilter filter;
     }
 
+
+
     private static class ViewHolder {
         ToggleButton stockCollectBtn;
         TextView stockCode;
         TextView stockName;
     }
+
+    private OnKeyboardActionListener onKeyboardActionListener = new OnKeyboardActionListener() {
+
+        @Override
+        public void onPress(int primaryCode) {
+
+        }
+
+        @Override
+        public void onRelease(int primaryCode) {
+
+        }
+
+        @Override
+        public void onKey(int primaryCode, int[] keyCodes) {
+
+            // Get the EditText and its Editable
+            View focus = getWindow().getCurrentFocus();
+            if(null == focus || EditText.class == focus.getClass()) {
+                return;
+            }
+
+            EditText editText = (EditText) focus;
+            Editable editable = editText.getText();
+            int start = editText.getSelectionStart();
+
+            boolean insert = true;
+            String data = null;
+
+            if(KEY_600 == primaryCode) {
+                data = getString(R.string.key_600);
+            } else if(KEY_000 == primaryCode) {
+                data = getString(R.string.key_000);
+            } else if(KEY_300 == primaryCode) {
+                data = getString(R.string.key_300);
+            } else if(KEY_002 == primaryCode) {
+                data = getString(R.string.key_002);
+            } else if(KEY_DEL == primaryCode) {
+                if(null != editable && 0 < start) {
+                    editable.delete(start-1, start);
+                }
+                insert = false;
+            } else if(KEY_ABC == primaryCode) {
+                insert = false;
+                mKeyboard.setKeyboard(qwerty);
+            } else if(KEY_OK == primaryCode) {
+                insert = false;
+
+            } else if(KEY_HID == primaryCode) {
+                insert = false;
+                hideKeyboard();
+            } else if(KEY_XXX == primaryCode) {
+                insert = false;
+
+            } else if(KEY_123 == primaryCode) {
+                insert = false;
+                mKeyboard.setKeyboard(symbols);
+            } else {
+                data = Character.toString((char) primaryCode);
+            }
+
+            if(null != editable && insert) {
+                editable.insert(start, data);
+            }
+        }
+
+        @Override
+        public void onText(CharSequence text) {
+
+        }
+
+        @Override
+        public void swipeLeft() {
+
+        }
+
+        @Override
+        public void swipeRight() {
+
+        }
+
+        @Override
+        public void swipeDown() {
+
+        }
+
+        @Override
+        public void swipeUp() {
+
+        }
+
+        private final static int KEY_600 = 9901;
+        private final static int KEY_000 = 9902;
+        private final static int KEY_300 = 9903;
+        private final static int KEY_002 = 9904;
+        private final static int KEY_DEL = 9905;
+        private final static int KEY_ABC = 9906;
+        private final static int KEY_OK  = 9907;
+        private final static int KEY_HID = 9908;
+        private final static int KEY_XXX = 9909;
+        private final static int KEY_123 = 9910;
+    };
+
+    private KeyboardView mKeyboard;
+    private Keyboard symbols;
+    private Keyboard qwerty;
+
 }
